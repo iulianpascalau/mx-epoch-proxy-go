4e4eeb0d4cdde61c8aa320d0368f1c862febe5f4
Iulian Pascalau <iulian.pascalau@gmail.com>
2026-01-16 14:44:52 +0200

    - completed the main file - minor fixes in process package

---
+++ services/crypto-payment/config/tomlConfig_test.go
@@ -0,0 +1,53 @@
+package config
+
+import (
+	"testing"
+
+	"github.com/pelletier/go-toml"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConfig(t *testing.T) {
+	t.Parallel()
+
+	testString := `
+Port = 8080
+
+# WalletURL represents the wallet HTTP address
+WalletURL = "devnet-wallet.multiversx.com"
+
+# ExplorerURL represents the explorer HTTP address
+ExplorerURL = "devnet-explorer.multiversx.com"
+
+# ProxyURL represents the proxy URL for the API
+ProxyURL = "https://devnet-gateway.multiversx.com"
+
+ContractAddress = "erd1qqqqqqqqqqqqqpgqc6u0p4kfkr5ekcrae86m6knx46gr36khrqqqhf96zw"
+
+CallSCGasLimit = 40000000
+
+SCSettingsCacheInSeconds = 60
+
+MinimumBalanceToProcess = 0.01
+
+TimeToProcessAddressesInSeconds = 60
+`
+
+	expectedCfg := Config{
+		Port:                            8080,
+		WalletURL:                       "devnet-wallet.multiversx.com",
+		ExplorerURL:                     "devnet-explorer.multiversx.com",
+		ProxyURL:                        "https://devnet-gateway.multiversx.com",
+		ContractAddress:                 "erd1qqqqqqqqqqqqqpgqc6u0p4kfkr5ekcrae86m6knx46gr36khrqqqhf96zw",
+		CallSCGasLimit:                  40000000,
+		SCSettingsCacheInSeconds:        60,
+		MinimumBalanceToProcess:         0.01,
+		TimeToProcessAddressesInSeconds: 60,
+	}
+
+	cfg := Config{}
+
+	err := toml.Unmarshal([]byte(testString), &cfg)
+	assert.Nil(t, err)
+	assert.Equal(t, expectedCfg, cfg)
+}
